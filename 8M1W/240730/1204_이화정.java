adcf345ad14c03b82d0be7679d67a04775726504
hwajeonge <wlrmadldi0516@naver.com>
2024-07-30 13:48:29 +0900

    1204_이화정

---
+++ 8M1W/240730/1204_이화정.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			int count = sc.nextInt();
+			int[] arr = new int[100];
+			
+			for(int i = 0; i<100; i++) {
+				arr[i] = sc.nextInt();
+			}
+			
+
+
+		}
+	}
+}
