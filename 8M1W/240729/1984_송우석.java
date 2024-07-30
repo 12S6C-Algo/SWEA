c17520a2c3800e803158f27c5ba97b2fcc19d1c6
송우석 <108509935+PlutoWooSeok@users.noreply.github.com>
2024-07-30 13:49:16 +0900

    Create 1984_송우석.java

---
+++ 8M1W/240729/1984_송우석.java
@@ -0,0 +1,57 @@
+package APS.Day01;
+/*
+10개의 수를 입력 받아, 최대 수와 최소 수를 제외한 나머지의 평균값을 출력하는 프로그램을 작성하라.
+
+(소수점 첫째 자리에서 반올림한 정수를 출력한다.)
+
+
+[제약 사항]
+
+각 수는 0 이상 10000 이하의 정수이다.
+
+
+[입력]
+
+가장 첫 줄에는 테스트 케이스의 개수 T가 주어지고, 그 아래로 각 테스트 케이스가 주어진다.
+
+각 테스트 케이스의 첫 번째 줄에는 10개의 수가 주어진다.
+
+
+[출력]
+
+출력의 각 줄은 '#t'로 시작하고, 공백을 한 칸 둔 다음 정답을 출력한다.
+
+(t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 중간_평균값_구하기 {
+    public static void main(String[] args)throws IOException {
+      
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            int[] arr = new int[10];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 10; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+
+            int mid_sum = 0;
+            for (int j = 1; j < 9; j++) {
+                mid_sum += arr[j];
+            }
+
+            double midSumAvg = mid_sum / 8.0;
+            int result = (int) Math.round(midSumAvg);
+            System.out.printf("#%d %d\n", i, result);
+        }
+    }
+}
