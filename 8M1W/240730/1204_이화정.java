c7d5f3b97c68a71ec102b74cdb5747c78e30c709
hwajeonge <wlrmadldi0516@naver.com>
2024-07-30 14:56:29 +0900

    0730_이화정

--- 8M1W/240730/1204_이화정.java
+++ 8M1W/240730/1204_이화정.java
@@ -1,29 +1,59 @@
-package practice;
+package 이화정_0730;
 
-import java.util.Arrays;
 import java.util.Scanner;
 import java.io.FileInputStream;
 
-class Solution
-{
+
+class Solution3 {
 	public static void main(String args[]) throws Exception
 	{
-		
+
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-		
+
+
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{	
 			int count = sc.nextInt();
-			int[] arr = new int[100];
+			int arr[] = new int[100];
 			
 			for(int i = 0; i<100; i++) {
 				arr[i] = sc.nextInt();
 			}
 			
+			int maxidx = 0;
+			int minidx = 0;
+			for (int i = 0; i<count; i++) {
+				maxidx = 0;
+				minidx = 0;
+				int max = arr[0];
+				int min = arr[0];
+				for(int j = 1; j<100; j++) {
+					if (max < arr[j]) {
+						max = arr[j];
+						maxidx = j;
+					}
+					if (min > arr[j]) {
+						min = arr[j];
+						minidx = j;
+					}
+				}
+				arr[maxidx] -= 1;
+				arr[minidx] += 1;				
+			}
 
+			int max = arr[0];
+			int min = arr[0];
+			for(int j = 1; j<100; j++) {
+				if (max < arr[j]) {
+					max = arr[j];
+				}
+				if (min > arr[j]) {
+					min = arr[j];
+				}
+			}
+			
+
+			System.out.println("#" + test_case + " " + (max-min));
 
 		}
 	}
---
+++ 8M1W/240730/1208_이화정.java
@@ -0,0 +1,48 @@
+package 이화정_0730;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution2 {
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			int number = sc.nextInt();
+			int[] arr = new int[1000];
+			
+			for(int i = 0; i<1000; i++) {
+				arr[i] = sc.nextInt();
+			}
+			
+			int[] count = new int[101];
+			
+			for(int i = 0; i<1000; i++) { // i는 학생 번호
+				for(int j = 0; j<=100; j++) { // j는 점수
+					if(arr[i] == j) {
+						count[j] += 1;
+					}
+				}
+			}
+			// index가 점수이고, count안의 값이 점수의 개수
+			// 최소 개수의 점수를 찾아야 함!
+			int max = count[0];
+			int maxidx = 0;
+			for(int i = 1; i<101; i++) {
+				if(max <= count[i]) {
+					max = count[i];
+					maxidx = i;
+				}
+			}
+			System.out.println("#" + number + " " + maxidx);
+
+		}
+	}
+}
