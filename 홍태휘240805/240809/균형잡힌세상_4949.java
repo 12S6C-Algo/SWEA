1443b8d3671fedc56b9a289976ea2796321a3f1c
Hongssi96 <paul960801@gmail.com>
2024-08-09 07:31:49 +0900

    Update 균형잡힌세상_4949.java

--- 홍태휘240805/240809/균형잡힌세상_4949.java
+++ 홍태휘240805/240809/균형잡힌세상_4949.java
@@ -8,15 +8,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		while (true) {
+			// 글자를 받음
 			String str = br.readLine();
-			
+			// 온점 "."만 있는 줄을 만나면 종료
 			if ( str.equals(".")) 
 				break;
-			
+			// 결과 값을 위한 boolean
 			boolean isBalanced = true;
+			// 스택으로 풀거라 스택 만듦
 			Stack<Character> stack = new Stack<>();
 			
 			for ( int i = 0 ; i < str.length() ; i++) {
+				// 다음 글자에 따라서 stack에 저장을 할지, stack에서 뽑아서 검사해서 결과를 볼지 정함
 				char c = str.charAt(i);
 				if ( c == '(' || c == '{' || c == '[') {
 					stack.push(c);
@@ -35,7 +38,7 @@ public class Main {
 					}
 				}
 			}
-			
+			// 괄호의 수가 맞지 않으면 남아있기 때문에 확인해줌
 			if (!stack.isEmpty()) {
 				isBalanced = false;
 			}
